cc0618ac8017b86a199757f7669a0ae12fb3d12d
Prabhat Garg <prabhat219@outlook.com>
2021-03-23 11:29:49 +0530

    First

---
+++ PlayerHandling.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerTesting
+{
+    [TestFixture]
+    class PlayerHandling
+    {
+        [Test]
+        public void TestPlayer()
+        {
+            foreach (var player in Program.playerList())
+            {
+                Assert.NotNull(player.PlayerId);
+                Assert.NotNull(player.PlayerName);
+                Assert.NotNull(player.PlayerTeam);
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerTesting
+{
+    class Program
+    {
+        public static List<Player> playerList()
+        {
+            List<Player> listPlayers = new List<Player>
+            {
+                new Player{PlayerId=1,PlayerName="Ronaldo",PlayerTeam="Portugal"},
+                new Player{PlayerId=2,PlayerName="Mbappe",PlayerTeam="France"},
+                new Player{PlayerId=3,PlayerName="Messi",PlayerTeam="Argentina"},
+            };
+            return listPlayers;
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
